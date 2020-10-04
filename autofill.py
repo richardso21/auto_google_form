5fd6d96aa3abaa6c521b8ed643cb3710496e7da9
richardso21 <richard.so21@sitechhs.com>
2020-10-04 10:36:05 -0400

    formatting

--- autofill.py
+++ autofill.py
@@ -5,43 +5,45 @@ import json
 import os
 from urllib import parse
 
+
 def main():
-  if not os.path.exists('creds.json'):
-    print('MAKE YOUR `creds.json` FILE FIRST!!!')
-    return
+    if not os.path.exists('creds.json'):
+        print('MAKE YOUR `creds.json` FILE FIRST!!!')
+        return
 
-  with open('creds.json') as F:
-    data = json.load(F)
+    with open('creds.json') as F:
+        data = json.load(F)
 
+    url = f'https://docs.google.com/forms/d/e/{data["FORM_ID"]}/formResponse?'
 
-  url = f'https://docs.google.com/forms/d/e/{data["FORM_ID"]}/formResponse?'
+    params = data["entries"]
+    keys = list(params.keys())
+    for x in keys:
+        params[f"entry.{x}"] = params.pop(x)
 
-  params = data["entries"]
-  keys = list(params.keys())
-  for x in keys:
-    params[f"entry.{x}"] = params.pop(x)
+    url_query = parse.urlencode(params)
+    new_url = url + url_query
 
-  url_query = parse.urlencode(params)
-  new_url = url + url_query
+    browser = webdriver.Firefox()
+    browser.get(new_url)
+    time.sleep(1)
 
-  browser = webdriver.Firefox()
-  browser.get(new_url)
-  time.sleep(1)
+    elem = browser.find_element_by_id('identifierId')
+    elem.send_keys(data["email"] + Keys.RETURN)
 
-  elem = browser.find_element_by_id('identifierId')
-  elem.send_keys(data["email"] + Keys.RETURN)
+    time.sleep(2)
 
-  time.sleep(2)
+    elem = browser.find_element_by_name('password')
+    elem.send_keys(data["password"] + Keys.RETURN)
+    time.sleep(2)
 
-  elem = browser.find_element_by_name('password')
-  elem.send_keys(data["password"] + Keys.RETURN)
-  time.sleep(2)
+    elem = browser.find_element_by_class_name(
+        'appsMaterialWizButtonPaperbuttonLabel')
+    elem.click()
+    time.sleep(1)
 
-  elem = browser.find_element_by_class_name('appsMaterialWizButtonPaperbuttonLabel')
-  elem.click()
-  time.sleep(1)
+    browser.close()
 
-  browser.close()
 
 if __name__ == "__main__":
     main()
